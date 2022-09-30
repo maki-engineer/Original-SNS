4a7a2cef7598ae51a0d65f9065db08024a6a5f7b
o2yama <taisei.otsuyama@gmail.com>
2022-09-30 22:52:37 +0900

    add: accountsテーブル作成

---
+++ database/migrations/2022_09_30_220340_create_accounts_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('accounts', function (Blueprint $table) {
+            $table->id('account_id');
+            $table->string('name');
+            $table->tinyInteger('age');
+            $table->string('icon_image_path');
+            $table->tinyInteger('active_status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('accounts');
+    }
+};
