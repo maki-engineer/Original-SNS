703dc258bf6ac7401e1642a5b107c68af93a4b6a
o2yama <taisei.otsuyama@gmail.com>
2022-09-30 23:19:41 +0900

    delete: usersテーブルのaccount_registration_statusカラム削除 account_registration_statusのEnumクラス削除とRegisteredUserController, UserModelの修正

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers\Auth;
 
-use App\Enums\AccountRegistrationStatus;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use App\Providers\RouteServiceProvider;
@@ -44,7 +43,6 @@ class RegisteredUserController extends Controller
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
-            'account_registration_status' => AccountRegistrationStatus::ONLY_USER_REGISTRATION,
         ]);
 
         event(new Registered($user));
