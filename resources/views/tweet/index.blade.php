70ef15d2c4f0b83946da225f242849054a296eb1
maki-engineer <ryohei-81@outlook.jp>
2022-10-08 10:36:06 +0900

    update: ログインボタンと会員登録ボタンを実装

--- resources/views/tweet/index.blade.php
+++ resources/views/tweet/index.blade.php
@@ -61,6 +61,15 @@
               </form>
           </div>
         @endauth
+
+        @guest
+          <div class="flex flex-wrap justify-center">
+            <div class="w-1/2 p-4 flex flex-wrap justify-evenly">
+              <x-element.button-a :href="route('login')">ログイン</x-element.button-a>
+              <x-element.button-a :href="route('register')" theme="secondary">会員登録</x-element.button-a>
+            </div>
+          </div>
+        @endguest
       </div>
     </div>
 
