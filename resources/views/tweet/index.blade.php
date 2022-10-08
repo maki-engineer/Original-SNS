eb2738b4e4cb9bd6fbff1a0c8ec3175a1a9d0241
maki-engineer <ryohei-81@outlook.jp>
2022-10-08 10:22:25 +0900

    update: ログアウトボタン実装

--- resources/views/tweet/index.blade.php
+++ resources/views/tweet/index.blade.php
@@ -11,6 +11,17 @@
 <body>
     <div class="flex justify-center">
       <div class="max-w-screen-sm w-full">
+        @auth
+          <form action="{{ route('logout') }}" method="post">
+            @csrf
+            <div class="flex justify-end p-4">
+              <button class="mt-2 text-sm text-gray-500 hover:text-gray-800"
+                      onclick="event.preventDefault(); this.closest('form').submit();">
+                ログアウト
+              </button>
+            </div>
+          </form>
+        @endauth
         <h2 class="text-center text-blue-500 text-4xl font-bold mt-8 mb-8">original-sns</h2>
         @auth
           <div class="p-4">
