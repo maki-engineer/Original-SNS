9d90ee13ae941501c3bbd9a9ff29643f65883405
maki-engineer <ryohei-81@outlook.jp>
2022-10-17 22:50:32 +0900

    update: goodsテーブル作成

---
+++ database/migrations/2022_10_17_221245_create_goods_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('goods', function (Blueprint $table) {
+            $table->unsignedBigInteger('tweet_id');
+            $table->foreign('tweet_id')->references('tweet_id')->on('tweets')->cascadeOnDelete();
+            $table->foreignId('user_id')->constrained('accounts')->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('goods');
+    }
+};
