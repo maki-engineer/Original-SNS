3a45c7d44ff93555bd7c7c746deddd7cb4490911
maki-engineer <ryohei-81@outlook.jp>
2022-11-22 10:03:12 +0900

    update: 登録ボタンの背景を削除

--- resources/views/components/register_account.blade.php
+++ resources/views/components/register_account.blade.php
@@ -27,7 +27,7 @@
         </div>
     </div>
     <small class="font-light text-gray-700">※チェックすると「10 ~ 20歳」のように表示されます</small>
-    <div class="bg-gray-50 px-4 py-3 text-right sm:p-6">
+    <div class="px-4 py-3 text-right sm:p-6">
         <button type="submit"
             class="inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">登録</button>
     </div>
