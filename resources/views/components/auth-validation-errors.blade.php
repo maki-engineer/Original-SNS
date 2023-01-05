f8920ba5f6ec14bdcce43420c704d4491fb76a1f
t.o2yama <taisei.otsuyama@gmail.com>
2023-01-06 07:26:07 +0900

    feature: ログイン画面（+パスワード再設定リンク送信画面）のUI実装

--- resources/views/components/auth-validation-errors.blade.php
+++ resources/views/components/auth-validation-errors.blade.php
@@ -2,11 +2,7 @@
 
 @if ($errors->any())
     <div {{ $attributes }}>
-        <div class="font-medium text-red-600">
-            {{ __('Whoops! Something went wrong.') }}
-        </div>
-
-        <ul class="mt-3 list-disc list-inside text-sm text-red-600">
+        <ul class="mt-3 list-none list-inside text-sm text-red-600">
             @foreach ($errors->all() as $error)
                 <li>{{ $error }}</li>
             @endforeach
--- resources/views/components/input.blade.php
+++ resources/views/components/input.blade.php
@@ -1,3 +1,6 @@
 @props(['disabled' => false])
 
-<input {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50']) !!}>
+<input {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge([
+    'class' =>
+        'rounded-md shadow-sm border-gray-200 bg-gray-100 focus:border-green-300 focus:ring-5 focus:ring-green-200 focus:ring-opacity-50 focus:bg-white focus:border-green-200',
+]) !!}>
