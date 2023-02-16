0e0b02d6647add383bf3fafbd408a52452ab5159
t.o2yama <taisei.otsuyama@gmail.com>
2023-02-16 12:05:04 +0900

    fix: 自分のアカウント取得時に、active_statusがACTIVEのレコードのみ取得するように修正

--- app/Enums/AccountStatus.php
+++ app/Enums/AccountStatus.php
@@ -7,15 +7,15 @@ namespace App\Enums;
 use BenSampo\Enum\Enum;
 
 /**
- * @method static static LOGIN     
- * @method static static LOGOUT    
- * @method static static DELETED   
- * @method static static FROZEN    
+ * @method static static ACTIVE
+ * @method static static DEACTIVE
+ * @method static static DELETED
+ * @method static static FROZEN
  */
 final class AccountStatus extends Enum
 {
-    const LOGIN = 1; // ログイン済み
-    const LOGOUT = 2; // ログアウト中
+    const ACTIVE = 1; // アカウント利用中
+    const DEACTIVE = 2; // 違うアカウント利用中
     const DELETED = 0; // 削除済み
     const FROZEN = -1; // 凍結
 
@@ -27,10 +27,10 @@ final class AccountStatus extends Enum
      */
     public static function getDescription($value): string
     {
-        if ($value === self::LOGIN) {
+        if ($value === self::ACTIVE) {
             return 'ログイン中';
         }
-        if ($value === self::LOGOUT) {
+        if ($value === self::DEACTIVE) {
             return 'ログアウト中';
         }
         if ($value === self::DELETED) {
